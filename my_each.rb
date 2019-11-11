4e5a193f8043570a660e98986a5770eb565b3a62
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-11 15:43:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each(array)
   if block_given?
     i = 0 
     
-  while i < array.length 
-    yield array[i]
-    i = i + 1 
+    while i < array.length 
+      yield array[i]
+      i = i + 1 
   end
 end
